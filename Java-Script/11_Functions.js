16cb1643cab84b31d4336aad6ec156833576deb3
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2022-12-24 13:35:18 +0530

    Create 11_Functions.js

---
+++ Java-Script/11_Functions.js
@@ -0,0 +1,15 @@
+function add(a, b){
+  return a+b;
+}
+//new function creation in javascript
+const sum = (p,q) =>{
+  return p+q
+}
+
+let a=2
+let b=4
+let c=6
+console.log("Sum is",add(a,b));
+console.log("Sum is",add(a,c));
+console.log("Sum is",add(b,c));
+console.log(sum(a,b));
