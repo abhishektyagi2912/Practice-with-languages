f051510862918b12fcf594386a0a320c6648a583
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2022-12-24 13:22:47 +0530

    Create 05_Practice_Set-1.js

---
+++ Java-Script/05_Practice_Set-1.js
@@ -0,0 +1,17 @@
+// Q-1 create a variable of type stringand try to add a number to it
+let a= "abhishek"
+let b =6
+console.log(a+b)
+
+//Q-2 find the string type of in Q-1
+console.log(typeof (a+b))
+
+//Q-3 try to add new key in js object
+const item = {
+  "abhishek" : true,
+  "abhi": false,
+  "tyagi": 67,
+  "name":undefined
+}
+item['last']=50
+console.log(item)
