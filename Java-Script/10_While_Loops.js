7173c695d02e8d08ea30d9f8176352241137710d
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2022-12-24 13:33:46 +0530

    Create 10_While_Loops.js

---
+++ Java-Script/10_While_Loops.js
@@ -0,0 +1,22 @@
+//While loop
+/*
+let a = prompt("Enter the range")
+a = Number.parseInt(a)
+
+let i = 0
+while(i<a){
+  console.log(i)
+  i++
+}
+*/
+
+//do while loop
+
+let a = prompt("Enter the range")
+a = Number.parseInt(a)
+
+let i = 0
+do{
+  console.log(i)
+  i++
+}while(i < a)
