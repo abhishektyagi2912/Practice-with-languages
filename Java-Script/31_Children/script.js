5de719476e98d4b170ade4c19761d5b72603f2d8
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-01-14 22:14:12 +0530

    Create script.js

---
+++ Java-Script/31_Children/script.js
@@ -0,0 +1,4 @@
+console.log(document.body.firstChild)  //print hello world if div is here this print div or anything
+console.log(document.body.lastChild)
+let arr = Array.from(document.body.childNodes)
+console.log(arr)
