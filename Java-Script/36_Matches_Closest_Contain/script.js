37521328f1da33d09b9f1df3c7e6407fed7f741d
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-01-15 12:55:05 +0530

    Create script.js

---
+++ Java-Script/36_Matches_Closest_Contain/script.js
@@ -0,0 +1,7 @@
+let id1 = document.getElementById("id1")
+console.log(id1)
+console.log(id1.matches(".class")) //return false because no id1 has box class
+console.log(id1.matches(".box"))
+console.log(sp1.closest("#sp1"))
+console.log(sp1.contains(sp1))
+console.log(sp1.contains(id1))
