7c1001940da0e1c744585ea4c23d985c6d42c197
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-01-17 00:35:14 +0530

    Create script.js

---
+++ Java-Script/43_ClassName_ClassList/script.js
@@ -0,0 +1,7 @@
+first.className = "text-black red" // change the class name of id = first
+first.classList.add("red")
+first.classList.remove("red") // remove class name after execution of js see red background is remove
+let a = first.classList.contains("red")  // check whether it execute or not
+console.log(a)
+
+first.classList.toggle("red")   // agr h to hta do vrna lga do 
