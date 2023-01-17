97a1149717dbed970d602b36b1ba56fdca975c73
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-01-17 18:57:08 +0530

    Create script.js

---
+++ Java-Script/44_SetTimeout/script.js
@@ -0,0 +1,22 @@
+document.write("Hello")
+
+const sum = (a, b, c) => {
+  console.log("Yes I am running " + (a + b + c))
+  a + b
+}
+
+setTimeout(sum, 1000, 1, 2, 7)  // ek sec baad y sum ko print kr dega kitne bhi variable d skte h hum isme
+
+// setInterval(function() {
+//   alert("setinterval")
+// }, 3000)
+
+// let a = setTimeout(function() {
+//   alert("I am inside of settimeout")
+// }, 5000)
+
+// let b = prompt("Do you want to run the settimout?")
+// if ("n" == b) {
+//   clearTimeout(a)
+// }
+// console.log(a)
